f3b4502e796bfcdedb65c58cc6563052d3fb2c34
ggongdori <soominlee0410@gmail.com>
2022-05-13 23:59:13 +0900

    feat: redis login impl, refresh token in redis, logout to be implemented

--- src/main/java/com/example/finalprojectoauthlogin/advice/exception/LoginFailureException.java
+++ src/main/java/com/example/finalprojectoauthlogin/advice/exception/LoginFailureException.java
@@ -3,11 +3,9 @@ package com.example.finalprojectoauthlogin.advice.exception;
 public class LoginFailureException extends RuntimeException{
     public LoginFailureException() {
     }
-
     public LoginFailureException(String message) {
         super(message);
     }
-
     public LoginFailureException(String message, Throwable cause) {
         super(message, cause);
     }
